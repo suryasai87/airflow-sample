66d7e4b398ba23c9a0b1fd15321c4555c8603384
Surya Sai Turaga <16309578+suryasai87@users.noreply.github.com>
2021-01-26 10:27:48 -0500

    Update python_workflow.py

--- DAGS/python_workflow.py
+++ DAGS/python_workflow.py
@@ -4,7 +4,7 @@
 #!pip install pandas
 
 
-''''
+'''
 from __future__ import absolute_import, unicode_literals, print_function
 
 from airflow import DAG
@@ -88,5 +88,4 @@ FIPS_states_query = requests.get(url + '?get=NAME&for=state:*' + '&key=' + key)
 FIPS_states_table = parse_query(FIPS_states_query.text)
 #
 FIPS_states_table.to_csv('s3://synpuf/state_codes/FIPS_states_table.csv')
-''''
-
+'''
